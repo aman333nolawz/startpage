64dde5ef89c572ac362071ea23d70b915ece55c6
aman333nolawz <amananudhiu@gmail.com>
2022-09-16 19:17:34 +0530

    Added some bookmarks

--- scripts.js
+++ scripts.js
@@ -5,11 +5,16 @@
  * Search function
  */
 
-const searchInput = document.querySelector("#searchbar > input")
-const searchButton = document.querySelector("#searchbar > button")
+const searchInput = document.querySelector("#searchbar > input");
+const searchButton = document.querySelector("#searchbar > button");
 
-const lookup = {"/":"/","deepl":"https://deepl.com/","reddit":"https://reddit.com/","maps":"https://maps.google.com/"}
-const engine = "google"
+const lookup = {
+  "/": "/",
+  deepl: "https://deepl.com/",
+  reddit: "https://reddit.com/",
+  maps: "https://maps.google.com/",
+};
+const engine = "google";
 const engineUrls = {
   deepl: "https://www.deepl.com/translator#-/-/",
   duckduckgo: "https://duckduckgo.com/?q=",
@@ -17,78 +22,127 @@ const engineUrls = {
   google: "https://www.google.com/search?q=",
   startpage: "https://www.startpage.com/search?q=",
   youtube: "https://www.youtube.com/results?q=",
-}
+};
 
-const isWebUrl = value => {
+const isWebUrl = (value) => {
   try {
-    const url = new URL(value)
-    return url.protocol === "http:" || url.protocol === "https:"
+    const url = new URL(value);
+    return url.protocol === "http:" || url.protocol === "https:";
   } catch {
-    return false
+    return false;
   }
-}
+};
 
-const getTargetUrl = value => {
-  if (isWebUrl(value)) return value
-  if (lookup[value]) return lookup[value]
-  return engineUrls[engine] + value
-}
+const getTargetUrl = (value) => {
+  if (isWebUrl(value)) return value;
+  if (lookup[value]) return lookup[value];
+  return engineUrls[engine] + value;
+};
 
 const search = () => {
-  const value = searchInput.value
-  const targetUrl = getTargetUrl(value)
-  window.open(targetUrl, "_self")
-}
+  const value = searchInput.value;
+  const targetUrl = getTargetUrl(value);
+  window.open(targetUrl, "_self");
+};
 
-searchInput.onkeyup = event => event.key === "Enter" && search()
-searchButton.onclick = search
+searchInput.onkeyup = (event) => event.key === "Enter" && search();
+searchButton.onclick = search;
 
 /**
  * inject bookmarks into html
  */
 
-const bookmarks = [{"id":"vDhGA1yBr52qEOUx","label":"Social Media","bookmarks":[{"id":"S7m03vks35Fwi2rd","label":"YouTube","url":"https://youtube.com"},{"id":"R0MkbKq2kY1cG3L1","label":"Discord","url":"https://discord.com/app"},{"id":"cqEzoJx5Nie4zOSn","label":"Github","url":"https://github.com/"},{"id":"dMx7RyGmwpiTlF6Y","label":"Reddit","url":"https://reddit.com"}]},{"id":"Y7hJ0zWHkgeBby1c","label":"Productivity","bookmarks":[{"id":"FTLaRJfmVQ0SIwA6","label":"Keybr","url":"https://keybr.com"},{"id":"jt0fui3bO9ujLifl","label":"Monkey Type","url":"https://monkeytype.com"},{"id":"VaxTvi0b7kVrU48T","label":"Image Enlarger","url":"https://bigjpg.com/en"}]},{"id":"CtLx8NctjGoab64f","label":"CTF","bookmarks":[{"id":"oF9fwHeAQ3WgUKOV","label":"CTF Time","url":"https://ctftime.org"},{"id":"7FcUcaKBnE0ft1Yc","label":"Try Hack Me","url":"https://tryhackme.com/"},{"id":"qO7M0SHfI0LCsqOC","label":"Hack The Box","url":"https://www.hackthebox.com/"},{"id":"cc6JnJ8ESlEz0Ur1","label":"Hack Tricks","url":"https://book.hacktricks.xyz/welcome/readme"}]}]
+const bookmarks = [
+  {
+    label: "Social Media",
+    bookmarks: [
+      { label: "YouTube", url: "https://youtube.com" },
+      {
+        label: "Discord",
+        url: "https://discord.com/app",
+      },
+      { label: "Instagram", url: "https://instagram.com" },
+      { label: "Github", url: "https://github.com/" },
+      { label: "Reddit", url: "https://reddit.com" },
+    ],
+  },
+  {
+    label: "Productivity",
+    bookmarks: [
+      { label: "Keybr", url: "https://keybr.com" },
+      {
+        label: "Monkey Type",
+        url: "https://monkeytype.com",
+      },
+      {
+        label: "Image Enlarger",
+        url: "https://bigjpg.com/en",
+      },
+    ],
+  },
+  {
+    label: "CTF",
+    bookmarks: [
+      { label: "CTF Time", url: "https://ctftime.org" },
+      {
+        label: "Try Hack Me",
+        url: "https://tryhackme.com/",
+      },
+      {
+        label: "Hack The Box",
+        url: "https://www.hackthebox.com/",
+      },
+      {
+        label: "Hack Tricks",
+        url: "https://book.hacktricks.xyz/welcome/readme",
+      },
+      { label: "Cyber Chef", url: "https://gchq.github.io/CyberChef/" },
+    ],
+  },
+];
 
 const createGroupContainer = () => {
-  const container = document.createElement("div")
-  container.className = "bookmark-group"
-  return container
-}
+  const container = document.createElement("div");
+  container.className = "bookmark-group";
+  return container;
+};
 
-const createGroupTitle = title => {
-  const h2 = document.createElement("h2")
-  h2.innerHTML = title
-  return h2
-}
+const createGroupTitle = (title) => {
+  const h2 = document.createElement("h2");
+  h2.innerHTML = title;
+  return h2;
+};
 
 const createBookmark = ({ label, url }) => {
-  const li = document.createElement("li")
-  const a = document.createElement("a")
-  a.href = url
-  a.innerHTML = label
-  li.append(a)
-  return li
-}
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = url;
+  a.innerHTML = label;
+  li.append(a);
+  return li;
+};
 
-const createBookmarkList = bookmarks => {
-  const ul = document.createElement("ul")
-  bookmarks.map(createBookmark).forEach(li => ul.append(li))
-  return ul
-}
+const createBookmarkList = (bookmarks) => {
+  const ul = document.createElement("ul");
+  bookmarks.map(createBookmark).forEach((li) => ul.append(li));
+  return ul;
+};
 
 const createGroup = ({ label, bookmarks }) => {
-  const container = createGroupContainer()
-  const title = createGroupTitle(label)
-  const bookmarkList = createBookmarkList(bookmarks)
-  container.append(title)
-  container.append(bookmarkList)
-  return container
-}
+  const container = createGroupContainer();
+  const title = createGroupTitle(label);
+  const bookmarkList = createBookmarkList(bookmarks);
+  container.append(title);
+  container.append(bookmarkList);
+  return container;
+};
 
 const injectBookmarks = () => {
-  const bookmarksContainer = document.getElementById("bookmarks")
-  bookmarksContainer.append()
-  bookmarks.map(createGroup).forEach(group => bookmarksContainer.append(group))
-}
+  const bookmarksContainer = document.getElementById("bookmarks");
+  bookmarksContainer.append();
+  bookmarks
+    .map(createGroup)
+    .forEach((group) => bookmarksContainer.append(group));
+};
 
-injectBookmarks()
+injectBookmarks();
